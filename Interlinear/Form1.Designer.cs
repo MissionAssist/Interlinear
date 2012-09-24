d87110f04de5dc81c6cee5194dd29975f49429cf
Stephen Palmstrom <spalmstr1@gmail.com>
2012-09-24 09:47:50 +0100

    Start of day 24 September 2012

--- Interlinear/Form1.Designer.cs
+++ Interlinear/Form1.Designer.cs
@@ -28,12 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtLegacy = new System.Windows.Forms.TextBox();
+            this.btnGetLegacyFile = new System.Windows.Forms.Button();
+            this.btnSegmentLegacy = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.DefaultExt = "doc";
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Legacy file name";
+            // 
+            // txtLegacy
+            // 
+            this.txtLegacy.Location = new System.Drawing.Point(138, 53);
+            this.txtLegacy.Name = "txtLegacy";
+            this.txtLegacy.Size = new System.Drawing.Size(178, 20);
+            this.txtLegacy.TabIndex = 1;
+            // 
+            // btnGetLegacyFile
+            // 
+            this.btnGetLegacyFile.Location = new System.Drawing.Point(336, 49);
+            this.btnGetLegacyFile.Name = "btnGetLegacyFile";
+            this.btnGetLegacyFile.Size = new System.Drawing.Size(75, 23);
+            this.btnGetLegacyFile.TabIndex = 2;
+            this.btnGetLegacyFile.Text = "Browse";
+            this.btnGetLegacyFile.UseVisualStyleBackColor = true;
+            this.btnGetLegacyFile.Click += new System.EventHandler(this.btnGetLegacyFile_Click);
+            // 
+            // btnSegmentLegacy
+            // 
+            this.btnSegmentLegacy.Enabled = false;
+            this.btnSegmentLegacy.Location = new System.Drawing.Point(140, 93);
+            this.btnSegmentLegacy.Name = "btnSegmentLegacy";
+            this.btnSegmentLegacy.Size = new System.Drawing.Size(93, 52);
+            this.btnSegmentLegacy.TabIndex = 3;
+            this.btnSegmentLegacy.Text = "Segment Legacy File";
+            this.btnSegmentLegacy.UseVisualStyleBackColor = true;
+            this.btnSegmentLegacy.Click += new System.EventHandler(this.btnSegmentLegacy_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(704, 298);
+            this.Controls.Add(this.btnSegmentLegacy);
+            this.Controls.Add(this.btnGetLegacyFile);
+            this.Controls.Add(this.txtLegacy);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Interlinear comparison";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtLegacy;
+        private System.Windows.Forms.Button btnGetLegacyFile;
+        private System.Windows.Forms.Button btnSegmentLegacy;
     }
 }
 
--- Interlinear/Form1.cs
+++ Interlinear/Form1.cs
@@ -6,14 +6,77 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Data.Common;
+using Word = Microsoft.Office.Interop.Word;
+using Office = Microsoft.Office.Core;
 
 namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
     {
+        private Word.Application wrdLegacyDoc;
+ 
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void btnGetLegacyFile_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK )
+            {
+                txtLegacy.Text = openFileDialog1.FileName;
+                btnSegmentLegacy.Enabled = true;
+
+        };
+        }
+
+        private void btnSegmentLegacy_Click(object sender, EventArgs e)
+        {
+            wrdLegacyDoc = new Word.Application();
+            wrdLegacyDoc.Documents.Open(txtLegacy.Text);
+            wrdLegacyDoc.Visible = true;
+            wrdLegacyDoc.ActiveWindow.View.ReadingLayout = false;  // Make sure we are in edit mode
+            wrdLegacyDoc.Documents[1].Activate();
+            //wrdLegacyDoc.ActiveDocument.ReadOnlyRecommended = false;
+            //wrdLegacyDoc.ActiveDocument.Select();
+
+            CleanWordText(wrdLegacyDoc); // delete the shape
+
+            QuitWord();
+        }
+        private void CleanWordText(Microsoft.Office.Interop.Word.Application theDoc)
+        {
+         
+            theDoc.Selection.HomeKey(Word.WdUnits.wdStory, Word.WdMovementType.wdMove);
+
+            for (int ShapeCounter =1; ShapeCounter <= theDoc.ActiveDocument.Shapes.Count; ShapeCounter++)
+            {
+                
+                if (theDoc.ActiveDocument.Shapes[ShapeCounter].Type == Office.MsoShapeType.msoTextBox)
+                {
+                    theDoc.ActiveDocument.Shapes[ShapeCounter].ConvertToInlineShape();
+                    theDoc.ActiveDocument.Shapes[ShapeCounter].Delete();
+                }
+            }
+
+        }
+         private void QuitWord()
+            {
+                if (wrdLegacyDoc != null)
+                {
+                    object missing = Type.Missing;
+                    wrdLegacyDoc.Quit(ref missing, ref missing, ref missing);
+                    GC.Collect();
+                    GC.WaitForPendingFinalizers();
+                    GC.Collect();
+                    GC.WaitForPendingFinalizers();
+                    wrdLegacyDoc = null;
+
+                }
+
+            }
+ 
     }
-}
+
+};
